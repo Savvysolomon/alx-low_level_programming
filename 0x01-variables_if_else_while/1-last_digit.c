7b175b711c9197e50da1d3d3bd3bc722557c0c78
Savvysolomon <savvysolomonumoh@gmail.com>
2022-07-03 08:04:43 -0700

    program that prints the alphabet in lowercase, and then in uppercase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-* main - entry point 
-* return: always return 0
+/**
+*main - entry point 
+*return: always return 0
 */
 int main(void)
 {
@@ -14,11 +14,11 @@ n = rand() - RAND_MAX / 2;
 lastn = n % 10;
 if (lastn > 5)
 {
-printf("Last digit of %d is %d is greater than 5\n", n, lastn);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
-printf("Last digit of %d is %d is 0\n", n, lastn);
+printf("Last digit of %d is %d and is less than 0\n", n, lastn);
 }
 else
 {
@@ -26,3 +26,4 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
 }
 return (0);
 }
+
