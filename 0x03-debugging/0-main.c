d44e38927d73ba5d9b82ad35c17b9b6c43036764
Savvysolomon <savvysolomonumoh@gmail.comgit config --global user.email savvysolomonumoh@gmail.com>
2022-07-06 13:06:14 -0700

    Debugging task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,6 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
 * main - tests function that prints if integer is positive or negative
-=======
-* main - main
->>>>>>> 8f518158e59185f91f69e9101f6538cd5306f1b9
 * Return: 0
 */
 int main(void)
@@ -14,7 +10,4 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-<<<<<<< HEAD
 
-=======
->>>>>>> 8f518158e59185f91f69e9101f6538cd5306f1b9
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,11 +2,5 @@
 #define MAIN_H
 #include <stdio.h>
 void positive_or_negative(int i);
-<<<<<<< HEAD
 #endif /* MAIN_H */
-=======
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-#endif
->>>>>>> 8f518158e59185f91f69e9101f6538cd5306f1b9
+
