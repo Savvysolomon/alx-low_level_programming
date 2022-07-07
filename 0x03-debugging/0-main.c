7e1446b3c3c0474c33b82fc2d87ba95c54b4e7e0
SOLOMON UMOH <104275781+Savvysolomon@users.noreply.github.com>
2022-07-07 14:34:39 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
 * main - main
@@ -11,20 +10,3 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-=======
-#include "main.h"
-/**
-*  main - tests function that prints if integer is positive or negative
-*  Return: 0
-*/
-int main(void)
-{
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
-<<<<<<< HEAD
-=======
->>>>>>> 99b8483c462286552fd0bb1a26319167e3f06ccd
->>>>>>> f37b726b12e40cd7211d8136d7c540a32d5f0917
