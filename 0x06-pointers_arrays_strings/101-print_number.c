6ca5e0971f8a689cc43419b3b4ee1b1ab6181f80
Savvysolomon <savvysolomonumoh@gmail.com>
2022-07-14 12:29:50 -0700

    A function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,45 @@
 #include "main.h"
-
 /**
-* print_number - print an integer, without using long, arrays, or pointers
-* @n: number to be printed
+* print_number - print an int numbers.
+* @n: number tested
+* Return: Always 0.
 */
 void print_number(int n)
 {
+int i, j, digit, digits, power;
+unsigned int temp, numchar, number;
 
-unsigned int tens, digit, positive = n;
-double t_beg = 1;
-if (n == 0)
-_putchar('0');
-else
-{
+digit = 0;
 if (n < 0)
 {
-positive = n * -1;
 _putchar('-');
+temp = -n;
 }
-while (t_beg <= positive)
-t_beg *= 10;
-tens = t_beg / 10;
-while (tens >= 1)
+else
 {
-digit = positive / tens;
-_putchar(digit + '0');
-positive = (positive - (tens * digit));
-tens /= 10;
+temp = n;
 }
+number = temp;
+
+while (number >= 10)
+{
+number = number / 10;
+digit++;
+}
+digits = digit + 1;
+power = 1;
+i = 1;
+
+while (i < digits)
+{
+power = power * 10;
+i++;
+}
+j = power;
+while (j >= 1)
+{
+numchar = (temp / j) % 10;
+_putchar(numchar + '0');
+j = j / 10;
 }
 }
