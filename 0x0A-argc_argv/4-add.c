ba787571d87ec7b2b826cfcdbd64de6f12318ef4
Savvysolomon <savvysolomonumoh@gmail.com>
2022-07-22 16:46:01 -0700

    A program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,16 @@
 #include <stdio.h> /* printf */
 #include <stdlib.h> /* atoi */
 #include <stdbool.h> /* bool data type */
+
 /**
 * is_num - iterate through each argv to test if it's a number
 * @argvv: a argv
 * Return: true only if entire string is a number, false if not
 */
+
 bool is_num(char *argvv)
 {
+
 int j = 0;
 for (j = 0; argvv[j]; j++)
 {
@@ -16,22 +19,26 @@ return (0);
 }
 return (1);
 }
+
 /**
 * main - print sum if all arguments given are numbers
 * @argc: argument counter
 * @argv: arguments
 * Return: 0 on success, 1 if an argument wasn't a number
 */
+
 int main(int argc, char *argv[])
 {
 int i = 1;
 int sum = 0;
+
 /* validate input */
 if (argc == 1)
 {
 printf("0\n");
 return (0);
 }
+
 /* check all arguments to add numbers */
 while (i < argc)
 {
@@ -45,6 +52,7 @@ return (1);
 i++;
 {
 printf("%d\n", sum);
+
 return (0);
 }
 
