83561d0c7f9e10a6a5ea2e1734acb3ccb6f23f64
Savvysolomon <savvysolomonumoh@gmail.com>
2022-07-22 15:37:43 -0700

    A program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,10 @@
 * Return: 0 on success
 */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 if(argc > 0)
-printf("program's name is: %s\n",argv[0]);
+printf("%s\n", argv[0]);
 return 0;
 }
 
