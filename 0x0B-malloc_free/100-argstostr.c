311f55e95b11d895d291bce0f834f60aed137440
Savvysolomon <savvysolomonumoh@gmail.com>
2022-07-26 16:45:16 -0700

    A function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,39 +1,61 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
-* argstostr - concatenates all the arguments of your program.
-* @ac: number of arguments
-* @av: double pointer to arguments
-* Return:pointer to new string, or NULL if error
+* _strcat - Concat string, src appended to dest
+* @dest: Destination string
+* @src: Source string
+* Return: Concat newString to dest
+*/
+char *_strcat(char *dest, char *src)
+{
+int a, b;
+
+for (a = 0; dest[a] != '\0'; a++)
+;
+for (b = 0; src[b] != '\0'; b++)
+{
+dest[a] = src[b];
+a++;
+}
+return (dest);
+}
+/**
+* _strlen - Return the number of the length of the string
+* @s: This is the string to check
+* Return: Return the length of the string
+*/
+int _strlen(char *s)
+{
+int a;
+for (a = 0; s[a] != '\0'; a++)
+;
+return (a);
+}
+/**
+* argstostr - Concats argv to new string
+* @ac: Argument count
+* @av: Arguments
+ Return: Returns a pointer to a new string, or NULL if it fails
 */
 char *argstostr(int ac, char **av)
 {
-int i, j, k = 0, n = 0;
-char *s;
 
-if (ac <= 0 || av == NULL)
+int index, sumString;
+char *newString;
+sumString = 1;
+if (ac == 0 || av == NULL)
+return (0);
+for (index = 0; index < ac && av[index]; index++)
+{
+sumString += _strlen(av[index]);
+}
+newString = malloc((sumString * sizeof(char)) + ac);
+for (index = 0; index < ac && av[index]; index++)
+{
+newString = _strcat(newString, av[index]);
+newString = _strcat(newString, "\n");
+}
+if (newString == NULL)
 return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-n++;
-n++;
-}
-n++;
-s = malloc(n * sizeof(char));
-if (s == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-s[k] = av[i][j];
-k++;
-}
-s[k] = '\n';
-k++;
-}
-s[k] = '\0';
-return (s);
+return (newString);
 }
